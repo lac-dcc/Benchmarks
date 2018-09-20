39eb3c36dd9a19b4dc4d998d1020021f615b5ab2
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-09-20 11:24:23 +0200

    add t2.c

---
+++ test/t2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int c = 0;
+
+int main(int argc, char* argv[]){
+  
+  int a = argv[0];
+
+  c = atoi(argv[0]);
+
+  int b = atoi(argv[2]);
+
+  // for (int i=0; i<atoi(argv[1]); i++){
+  //   a = a + i;
+  //   for (int j=0; j<atoi(argv[1]); j++){
+  //     a = a + j;
+  //   }
+  // }
+
+  b = b + a;
+
+  a = a + b;
+
+  c = a + c;
+  
+  printf("%d\n", a);
+  
+  return 0;
+}
