3dc5cfd147c20682bb267d457b7525c4600b2293
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-10-02 11:07:01 +0200

    upd

--- test/info.sh
+++ test/info.sh
@@ -4,6 +4,6 @@ prf_name="$bnc_name.ibc" ;
 obj_name="$bnc_name.o" ;
 exe_name="$bnc_name.exe" ;
 
-source_files=( "t1.c" )
+source_files=( "t10.c" )
 CXXFLAGS=" " ;
-RUN_OPTIONS=" " ;
+RUN_OPTIONS="5" ;
