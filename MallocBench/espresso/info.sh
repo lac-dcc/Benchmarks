700f7044a4d4f3fd4769f1a614183131a6809c7f
Breno Campos <brenocfg@gmail.com>
2019-12-18 21:24:41 -0300

    Fix espresso benchmark name (not expresso).

--- MallocBench/espresso/info.sh
+++ MallocBench/espresso/info.sh
@@ -1,4 +1,4 @@
-bench_name="expresso"
+bench_name="espresso"
 
 source_files=( "cofactor.c" "cols.c" "compl.c" "contain.c" "cubestr.c" "cvrin.c" "cvrm.c" "cvrmisc.c" "cvrout.c" "dominate.c" "equiv.c" "espresso.c" "essen.c" "exact.c" "expand.c" "gasp.c" "getopt.c" "gimpel.c" "globals.c" "hack.c" "indep.c" "irred.c" "main.c" "map.c" "matrix.c" "mincov.c" "opo.c" "pair.c" "part.c" "primes.c" "reduce.c" "rows.c" "set.c" "setc.c" "sharp.c" "sminterf.c" "solution.c" "sparse.c" "unate.c" "utility.c" "verify.c" )
 COMPILE_FLAGS=" -std=gnu89 -DNOMEMOPT "
